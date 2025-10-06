e9e668932b4b37c9ebabca27e6b54540320d5e1d
Rodrigo Martin Alvarez <martinrodrigoze@gmail.com>
2025-10-06 10:53:13 -0600

    Segundo paso

---
+++ Models/Vacantes.cs
@@ -0,0 +1,53 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+
+namespace WorkAroundSite.Models
+{
+    public class Vacantes
+    {
+        private readonly string _connectionString;
+
+        public Vacantes(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public List<Vacantes> GetVacantes()
+        {
+            List<Vacantes> lista = new List<Vacantes>();
+
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                string query = "SELECT Id, Titulo, Descripcion, Empresa, Ubicacion FROM Vacantes";
+                SqlCommand cmd = new SqlCommand(query, con);
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    lista.Add(new Vacantes
+                    {
+                        Id = (int)reader["Id"],
+                        Titulo = reader["Titulo"].ToString(),
+                        Descripcion = reader["Descripcion"].ToString(),
+                        Empresa = reader["Empresa"].ToString(),
+                        Ubicacion = reader["Ubicacion"].ToString()
+                    });
+                }
+            }
+
+            return lista;
+        }
+    }
+
+    public class Vacantes
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public string Empresa { get; set; }
+        public string Ubicacion { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using WorkAroundSite.Models;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddSingleton<Vacantes>();
+
 
 var app = builder.Build();
 
